55fa1cc8e47c8b7a8624892ea6e0292d28f32ae9
Marcus Hägerstrand <marcus.hagerstrand@gmail.com>
2021-12-21 15:53:35 +0100

    trying to connect foreign key id from lists table to task table

--- app/lists/store.php
+++ app/lists/store.php
@@ -11,13 +11,6 @@ if (isset($_POST['title'], $_POST['due_date'], $_POST['task_notes'])) {
     $dueDate = trim($_POST['due_date']);
     $taskNotes = trim($_POST['task_notes']);
 
-    // Insert $title, $dueDate and $taskNotes into tasks table
-    $statement = $database->prepare('INSERT INTO tasks(title, due_date, task_notes) VALUES(:title, :due_date, :task_notes)');
-    $statement->bindParam(':title', $title, PDO::PARAM_STR);
-    $statement->bindParam(':due_date', $dueDate, PDO::PARAM_INT);
-    $statement->bindParam(':task_notes', $taskNotes, PDO::PARAM_STR);
-    $statement->execute();
-
     // Connect users to their tasks by inserting user_id to lists table.
     $userID = $_SESSION['user']['id'];
 
@@ -29,13 +22,36 @@ if (isset($_POST['title'], $_POST['due_date'], $_POST['task_notes'])) {
     $statement->bindParam(':task_notes', $taskNotes, PDO::PARAM_STR);
     $statement->execute();
 
-    //SELF NOTE: I need to grab lists id and insert it into tasks
+    $statement = $database->prepare('SELECT * FROM lists WHERE id = :id');
+    $statement->bindParam(':id', $listId, PDO::PARAM_STR);
+    $statement->execute();
+
+    $list = $statement->fetch(PDO::FETCH_ASSOC);
+
+    $_SESSION['list'] = [
+        'id' => $list['id'],
+        'title' => $list['title'],
+        'due_date' => $list['due_date'],
+        'task_notes' => $list['task_notes'],
+    ];
+
+    $listID = $_SESSION['list']['id'];
+
+    // Insert $title, $dueDate and $taskNotes into tasks table
+    $statement = $database->prepare('INSERT INTO tasks(list_id, title, due_date, task_notes) VALUES(:list_id, :title, :due_date, :task_notes)');
+    $statement->bindParam(':list_id', $listID, PDO::PARAM_STR);
+    $statement->bindParam(':title', $title, PDO::PARAM_STR);
+    $statement->bindParam(':due_date', $dueDate, PDO::PARAM_INT);
+    $statement->bindParam(':task_notes', $taskNotes, PDO::PARAM_STR);
+    $statement->execute();
 
     $_SESSION['message'] = 'Great work! A new task has been added.';
-    redirect('/');
+    // Remove to show warnings
+    // redirect('/');
 } else {
     echo 'It\'s not working! :(';
+
     redirect('/new-task.php');
 }
 
-redirect('/');
+// redirect('/');
