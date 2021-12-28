f82b63ca010a7d4ff03952db5ffcbc6eea4d7233
Marcus Hägerstrand <marcus.hagerstrand@gmail.com>
2021-12-28 14:33:10 +0100

    Users can now edit username, email and password

--- app/users/update-profile.php
+++ app/users/update-profile.php
@@ -11,6 +11,7 @@ $email = $_SESSION['user']['email'];
 // If fields are unchanged while saving, display error message.
 if ($username === $_POST['name'] && $email === $_POST['email'] && empty($_POST['password']) && empty($_POST['password-new']) && empty($_POST['password-confirm'])) {
     $_SESSION['error'] = 'No changes has been made';
+    redirect('/profile.php');
 }
 
 // Check if the user has entered a different username
@@ -37,10 +38,10 @@ if ($username !== $_POST['name']) {
             redirect('/profile.php');
         }
         // If everything checks out update value
-        $updstatement = $database->prepare('UPDATE users SET name = :name WHERE id = :id');
-        $updstatement->bindParam(':name', $username, PDO::PARAM_STR);
-        $updstatement->bindParam(':id', $id, PDO::PARAM_INT);
-        $updstatement->execute();
+        $statement = $database->prepare('UPDATE users SET name = :name WHERE id = :id');
+        $statement->bindParam(':name', $username, PDO::PARAM_STR);
+        $statement->bindParam(':id', $id, PDO::PARAM_INT);
+        $statement->execute();
 
         // Updating session variable
         $_SESSION['user']['name'] = $username;
@@ -77,7 +78,9 @@ if ($email !== $_POST['email']) {
         redirect('/profile.php');
     }
 }
+
 if ($username === $_POST['name'] && $email === $_POST['email'] && !empty($_POST['password'])) {
+    // Change password
     if (isset($_POST['password'])) {
         $password = $_POST['password'];
 
@@ -110,51 +113,20 @@ if ($username === $_POST['name'] && $email === $_POST['email'] && !empty($_POST[
                 redirect('/profile.php');
             } else {
                 $password = password_hash($newPassphrase, PASSWORD_DEFAULT);
+
+                $statement = $database->prepare('UPDATE users SET password = :password WHERE id = :id');
+                $statement->bindParam(':password', $password, PDO::PARAM_STR);
+                $statement->bindParam(':id', $id, PDO::PARAM_INT);
+                $statement->execute();
+
                 $_SESSION['message'] = 'Your password has been updated';
                 redirect('/profile.php');
             }
-
-            $_SESSION['message'] = 'Your old password is correct';
-            redirect('/profile.php');
+            // $_SESSION['message'] = 'Your old password is correct';
+            // redirect('/profile.php');
         } else {
             $_SESSION['error'] = 'Your old password has been entered incorrectly.';
             redirect('/profile.php');
         }
     }
 }
-
-// if (isset($_POST['password'])) {
-//     $oldPassphrase = $_POST['password'];
-
-//     // Verify password before applying for a new password
-//     if (password_verify($oldPassphrase, $user['password'])) {
-//         if (isset($_POST['password-new'], $_POST['password-confirm'])) {
-//             $newPassphrase = $_POST['password-new'];
-//             $confirmPassphrase = $_POST['password-confirm'];
-
-//             // Validate passphrase requirements
-//             if (strlen($newPassphrase) < 8) {
-//                 $_SESSION['error'] = 'Your new password needs to be atleast 8 characters or longer';
-//                 redirect('/profile.php');
-//             }
-
-//             // Check if password matches, if true hash it
-//             if ($newPassphrase !== $confirmPassphrase) {
-//                 $_SESSION['error'] = 'Your password doesn\'t match, please try again';
-//                 redirect('/profile.php');
-//             } else {
-//                 $password = password_hash($newPassphrase, PASSWORD_DEFAULT);
-//                 $_SESSION['message'] = 'Your password has been updated';
-//                 redirect('/profile.php');
-//             }
-//             redirect('/profile.php');
-//         }
-//     } else {
-//         $_SESSION['error'] = 'Your old password has been entered incorrectly. Please enter it again.';
-//         redirect('/profile.php');
-//     }
-// }
-
-// If everything checks out update value
-// $_SESSION['message'] = 'Hello there';
-redirect('/profile.php');
