f322701915be2ae423db7e9f3ed48154f3d3aa12
Marcus Hägerstrand <marcus.hagerstrand@gmail.com>
2022-01-07 11:49:51 +0100

    lint update, will it pass my js now?

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,16 @@
-// const checkboxes = document.querySelectorAll('input[type=checkbox]');
+const checkboxes = document.querySelectorAll('input[type=checkbox]');
 
-// checkboxes.forEach((checkbox) => {
-//   checkbox.onclick = function () {
-//     this.parentNode.submit();
-//   };
-// });
+checkboxes.forEach((checkbox) => {
+  checkbox.onclick = function () {
+    this.parentNode.submit();
+  };
+});
 
-// const changeAvatar = (document.getElementById('avatar').onchange = function () {
-//   this.form.submit();
-// });
+const changeAvatar = (document.getElementById('avatar').onchange = function () {
+  this.form.submit();
+});
 
-// const removeAvatar = (document.getElementById('remove-avatar').onchange =
-//   function () {
-//     this.form.submit();
-//   });
+const removeAvatar = (document.getElementById('remove-avatar').onchange =
+  function () {
+    this.form.submit();
+  });
