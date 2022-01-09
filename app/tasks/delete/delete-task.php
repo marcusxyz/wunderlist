7fdc878112385e55a63b2016f4615731b3a058c7
Marcus Hägerstrand <marcus.hagerstrand@gmail.com>
2022-01-09 11:46:14 +0100

    Users can now delete tasks along with all their subtasks

---
+++ app/tasks/delete/delete-task.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+require __DIR__ . '/../../autoload.php';
+
+// If user is signed in, show session vairables
+if (!isset($_SESSION['user'])) {
+    redirect('/signin.php');
+}
+
+// In here users can delete tasks along with their subtasks
+
+print_r($_SESSION['task']);
+
+$taskID = $_SESSION['task']['id'];
+$task_name = $_SESSION['task']['task_name'];
+
+// First we delete all subtasks that belongs to the task
+$statement = $database->prepare('DELETE from lists WHERE task_id = :task_id');
+$statement->bindParam(':task_id', $taskID, PDO::PARAM_INT);
+$statement->execute();
+
+// Once all subtask is gone, delete the task.
+$statement = $database->prepare('DELETE from tasks WHERE id  = :id');
+$statement->bindParam(':id', $taskID, PDO::PARAM_INT);
+$statement->execute();
+
+$_SESSION['message'] = 'Task has been deleted';
+redirect('/');
--- edit-task.php
+++ edit-task.php
@@ -13,11 +13,9 @@ $editTask = editTasks($database);
 
 <!-- Edit user submitted tasks here -->
 <section class="edit-task-container">
+    <a href="/">Go back</a>
 
-    <div class="forms">
-        <h1><?= $editTask['task_name']; ?></h1>
-        <p>In here you can edit your task, add checklist, update it and delete your task.</p>
-        <button type="submit" name="submit-task" class="btn btn-half stroke">Mark as done</button>
+    <div class="message-container">
         <?php if ($error !== '') : ?>
             <p class="error"><?= $error; ?></p>
         <?php endif; ?>
@@ -25,71 +23,82 @@ $editTask = editTasks($database);
         <?php if ($message !== '') : ?>
             <p class="success"><?php echo $message; ?></p>
         <?php endif; ?>
-
-        <!-- <?php print_r($_SESSION['task']); ?>
-            <?php print_r($_SESSION['subtask']); ?> -->
-        <form action="app/tasks/update/update-task.php" method="post" required>
-            <div class="form">
-                <label for="task_name">Title</label>
-                <input type="text" name="task_name" id="task_name" value="<?= $editTask['task_name']; ?>" required>
-            </div>
-            <div class="form">
-                <label for="due_date">Due date</label>
-                <input type="datetime" name="due_date" id="due_date" placeholder="Please select a due date" value="<?= $editTask['due_date']; ?>" required>
-            </div>
-            <div class="form">
-                <label for="task_notes">Note</label>
-                <textarea type="text" name="task_notes" id="task_notes"><?= $editTask['task_notes']; ?></textarea>
-            </div>
-            <input type="hidden" name="id" id="<?= $task['id']; ?>">
-            <div class="button-container">
-                <button type="submit" name="submit-task" class="btn btn-half">Save changes</button>
-                <button type="submit" name="submit-task" class="btn btn-half delete">Delete task</button>
-            </div>
-        </form>
     </div>
-    <div class="forms">
-        <div class="subtask-container">
-            <h2>Create a checklist for your task</h2>
-            <form action="app/subtasks/create/create-subtask.php" method="post">
+
+    <div class="form-container">
+        <div class="forms">
+            <div class="task-info">
+                <h1><?= $editTask['task_name']; ?></h1>
+                <a href="" class="btn btn-stroke">Mark as done</a>
+            </div>
+            <p>In here you can edit your task, add checklist, update it and delete your task.</p>
+
+            <!-- <?php print_r($_SESSION['task']); ?>
+                <?php print_r($_SESSION['subtask']); ?> -->
+            <form action="app/tasks/update/update-task.php" method="post" required>
                 <div class="form">
-                    <label for="task_name">Add a task item</label>
-                    <input type="text" name="subtask_name" id="subtask_name" placeholder="Write something and press 'Enter'">
-                    <input type="hidden" name="id" id="<?= $task['id']; ?>">
-                    <input type="hidden" name="status" value="0">
-                    <button type="submit" name="submit-subtask" class="btn btn-auto">Add</button>
+                    <label for="task_name">Title</label>
+                    <input type="text" name="task_name" id="task_name" value="<?= $editTask['task_name']; ?>" required>
+                </div>
+                <div class="form">
+                    <label for="due_date">Due date</label>
+                    <input type="datetime" name="due_date" id="due_date" placeholder="Please select a due date" value="<?= $editTask['due_date']; ?>" required>
+                </div>
+                <div class="form">
+                    <label for="task_notes">Note</label>
+                    <textarea type="text" name="task_notes" id="task_notes"><?= $editTask['task_notes']; ?></textarea>
+                </div>
+                <input type="hidden" name="id" id="<?= $task['id']; ?>">
+                <div class="button-container">
+                    <button type="submit" name="submit-task" class="btn btn-half">Save changes</button>
+                    <a href="/app/tasks/delete/delete-task.php" class="btn btn-half delete">Delete task</a>
+
                 </div>
             </form>
-
-            <?php foreach (fetchSubTasks($database) as $subTask) : ?>
-                <div class="subtask-item">
-                    <form action="app/subtasks/update/update-subtask-status.php" method="post" class="subtask">
+        </div>
+        <div class="forms">
+            <div class="subtask-container">
+                <h2>Checklist</h2>
+                <form action="app/subtasks/create/create-subtask.php" method="post">
+                    <div class="form">
+                        <label for="task_name">Add a subtask</label>
+                        <input type="text" name="subtask_name" id="subtask_name" placeholder="Write something and press 'Enter'">
                         <input type="hidden" name="id" id="<?= $task['id']; ?>">
-                        <input type="hidden" name="subtask-id" value="<?= $subTask['id']; ?>">
-                        <input type="hidden" name="subtask-name" value="<?= $subTask['subtask_name']; ?>">
-                        <input type="hidden" name="subtask-status" value="<?= $subTask['status']; ?>">
-                        <?php if ($subTask['status'] == 1) : ?>
-                            <input type="checkbox" name="checkbox-toggle" id="checkbox-toggle" <?= 'checked="checked"' ?> />
-                            <p class="checked"><?= $subTask['subtask_name']; ?></p>
+                        <input type="hidden" name="status" value="0">
+                        <button type="submit" name="submit-subtask" class="btn btn-auto">Add</button>
+                    </div>
+                </form>
 
-                            <?= $subTask['id']; ?>
-                            <div>
-                                <a class="edit" href="/edit-subtask.php?id=<?= $subTask['id']; ?>&name=<?= $subTask['subtask_name']; ?>">Edit</a>
-                                <a class="delete" href="/app/subtasks/delete/delete-subtask.php?id=<?= $subTask['id']; ?>&name=<?= $subTask['subtask_name']; ?>">Delete</a>
-                            </div>
-                        <?php else : ?>
-                            <input type="checkbox" name="checkbox-toggle" id="checkbox-toggle" />
-                            <p><?= $subTask['subtask_name']; ?></p>
+                <?php foreach (fetchSubTasks($database) as $subTask) : ?>
+                    <div class="subtask-item">
+                        <form action="app/subtasks/update/update-subtask-status.php" method="post" class="subtask">
+                            <input type="hidden" name="id" id="<?= $task['id']; ?>">
+                            <input type="hidden" name="subtask-id" value="<?= $subTask['id']; ?>">
+                            <input type="hidden" name="subtask-name" value="<?= $subTask['subtask_name']; ?>">
+                            <input type="hidden" name="subtask-status" value="<?= $subTask['status']; ?>">
+                            <?php if ($subTask['status'] == 1) : ?>
+                                <input type="checkbox" name="checkbox-toggle" id="checkbox-toggle" <?= 'checked="checked"' ?> />
+                                <p class="checked"><?= $subTask['subtask_name']; ?></p>
 
-                            <?= $subTask['id']; ?>
-                            <div>
-                                <a href="/edit-subtask.php?id=<?= $subTask['id']; ?>&name=<?= $subTask['subtask_name']; ?>" class="edit">Edit</a>
-                                <a class="delete" href="/app/subtasks/delete/delete-subtask.php?id=<?= $subTask['id']; ?>&name=<?= $subTask['subtask_name']; ?>">Delete</a>
-                            </div>
-                        <?php endif; ?>
-                    </form>
-                </div>
-            <?php endforeach; ?>
+                                <?= $subTask['id']; ?>
+                                <div>
+                                    <a class="edit" href="/edit-subtask.php?id=<?= $subTask['id']; ?>&name=<?= $subTask['subtask_name']; ?>">Edit</a>
+                                    <a class="delete" href="/app/subtasks/delete/delete-subtask.php?id=<?= $subTask['id']; ?>&name=<?= $subTask['subtask_name']; ?>">Delete</a>
+                                </div>
+                            <?php else : ?>
+                                <input type="checkbox" name="checkbox-toggle" id="checkbox-toggle" />
+                                <p><?= $subTask['subtask_name']; ?></p>
+
+                                <?= $subTask['id']; ?>
+                                <div>
+                                    <a href="/edit-subtask.php?id=<?= $subTask['id']; ?>&name=<?= $subTask['subtask_name']; ?>" class="edit">Edit</a>
+                                    <a class="delete" href="/app/subtasks/delete/delete-subtask.php?id=<?= $subTask['id']; ?>&name=<?= $subTask['subtask_name']; ?>">Delete</a>
+                                </div>
+                            <?php endif; ?>
+                        </form>
+                    </div>
+                <?php endforeach; ?>
+            </div>
         </div>
     </div>
 </section>
